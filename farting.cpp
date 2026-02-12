91242c0b4e34de40bb99affd4abfaedef33431fb
jevt04 <jared.e.vantassell@gmail.com>
2026-02-11 18:43:56 -0600

    Create farting.cpp

---
+++ farting.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    string fart = "ouguughBRAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAp";
+    for (int lcv = 0; lcv < 255; lcv++)
+    {
+        cout << fart;
+    }
+    
+    return 0;
+}
